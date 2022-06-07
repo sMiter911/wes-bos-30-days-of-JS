c8329fa67cae17fccfbb6d685bf90cd4684451aa
Wandu <31390092+sMiter911@users.noreply.github.com>
2022-06-07 22:28:55 +0200

    WIP: Day 10

--- Day 9 - Console Tricks/js/scripts.js
+++ Day 9 - Console Tricks/js/scripts.js
@@ -36,7 +36,7 @@ const p = document.querySelector("p");
 console.assert(p.classList.contains("ouch"), "That is wrong!");
 
 // clearing
-console.clear();
+// console.clear();
 
 // Viewing DOM Elements
 console.log(p);
